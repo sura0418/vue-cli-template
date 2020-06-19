a3f08c82a9adbb6768ec4853123cf17255a67adb
sxd <503324901@qq.com>
2020-06-19 11:02:17 +0800

    'asd'

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-part">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: "home",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  computed: {},
+  props: {},
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/person/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="person-info">
+
+    </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: "index",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    
+  },
+  computed: {},
+  props: {},
+  methods: {},
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
